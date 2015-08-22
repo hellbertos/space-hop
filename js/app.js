5898702970b4ce2e5c7865f07fbbbeae464bb8b5
James R <james.ruggieri@yahoo.com>
2015-08-21 23:08:11 -0700

    Add Item Class with methods to enact bonus scoring objects

--- js/app.js
+++ js/app.js
@@ -27,7 +27,6 @@ Enemy.prototype.update = function(dt) {
            this.x = this.x;
            this.y = this.y;
         }
-
 };
 
 // Draw the enemy on the screen, required method for game
@@ -35,6 +34,67 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+// Item Class for bonus gems
+// Set up an array of possible positions
+var itemPosX = [0, 100, 200, 300, 400];
+var itemPosY = [60, 145];
+var howLongItem;
+
+var Item = function(x,y) {
+    this.x = x;
+    this.y = y;
+    this.sprite = 'images/gem-green.png';
+    this.active = false;
+    this.duration = 0;
+    this.count = 0;
+}
+
+Item.prototype.isSpawned = function(isActive) {
+    if(isActive === false) {
+        item.spawn();
+    }
+};
+
+Item.prototype.spawn = function() {
+    var spawnItem = Math.floor(Math.random()*10000);
+    if(spawnItem > 9950) {
+        // Set the active state of the item to true
+        this.active = true;
+
+        // Set duration of gem
+        this.duration = Math.floor(Math.random() * 70) + 60;
+
+        // Get random numbers to use as available x and y positions from array
+        var xCoord = Math.floor(Math.random() * 5);
+        var yCoord = Math.floor(Math.random() * 2);
+
+        //set coordinates
+        this.x = itemPosX[xCoord];
+        this.y = itemPosY[yCoord];
+        item.render();
+        }
+};
+
+Item.prototype.deActivate = function() {
+    this.x = -100;
+    this.count = 0;
+    this.active = false;
+};
+
+Item.prototype.update = function(howLongItem) {
+    if(gameState === true && this.active === true) {
+        // Generate a random number of cycles for gem to remain on screen
+        this.count += 1;
+        console.log("Let us try this.count -> "+this.count);
+        if(this.count === this.duration) {
+            item.deActivate();
+        }
+    }
+};
+
+Item.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -65,11 +125,17 @@ Player.prototype.update = function(xMod, yMod) {
                 this.y = this.y + yMod;
             }
         }
-
         // Check for a collision with enemies
-        player.collisiionDectect();
+        player.collisionDectect();
+
+        // Check to see if a gem is already spawned then update regardless
+        // and check for player collision with gem -> gemDetection()
+        item.isSpawned(item.active);
+        item.update();
+        player.gemDetection();
 
         // Check player position to see if they achived the top row
+        // to increase score and return avitar to beginning tile
         if (yMod && this.y === -13) {
             player.score();
             player.reset();
@@ -77,7 +143,6 @@ Player.prototype.update = function(xMod, yMod) {
     } else {
         player.render();
     }
-
 };
 
 Player.prototype.render = function() {
@@ -112,7 +177,7 @@ Player.prototype.handleInput = function(keyPress) {
 // Rewrite the the starting values of score and lives to
 // display in browser then call gameOn() to reset game state
 // to true which un-freezes the game
-Player.prototype.resetGameVars = function() {
+Player.prototype.resetGame = function() {
     this.points = 0;
     this.lives = 3;
     this.x = 200;
@@ -135,7 +200,7 @@ Player.prototype.replay = function() {
 
     var restartButton = document.getElementById('restartContain');
     restartButton.addEventListener('click', function(e) {
-            player.resetGameVars();
+            player.resetGame();
         });
 };
 
@@ -171,12 +236,21 @@ Player.prototype.score = function() {
 
 // Check for a collision between player and enemy.
 // Uses a range of -70 to +70 to account for the approximate width of the enemy
-Player.prototype.collisiionDectect = function() {
+Player.prototype.collisionDectect = function() {
     for(var i = 0; i < allEnemies.length; i++) {
         if ( ( Math.round( allEnemies[i].x) >= this.x -70 &&  Math.round( allEnemies[i].x) <= this.x + 70) &&  (this.y === Math.round( allEnemies[i].y) + 12 ) ) {
             player.loseLife();
             player.reset();
         }
+    }
+};
+
+// Check for collision with gem
+Player.prototype.gemDetection = function() {
+    if(this.x === item.x && this.y === item.y + 12) {
+        this.points += 50;
+        player.score();
+        item.deActivate();
     }
 };
 
@@ -200,19 +274,11 @@ Player.prototype.reset = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-/*var enemy1 = new Enemy(-1070, 60, 95);
-var enemy2 = new Enemy(-770, 145, 80);
-var enemy3 = new Enemy(0, 60, 60);
-var enemy4 = new Enemy(-100, 145, 60);
-var enemy5 = new Enemy(-300, 230, 60);*/
-//var enemy6 = new Enemy(100, 230, 150);
-
 var enemy1 = new Enemy( (Math.floor((Math.random() * -600) + 1) ),60, 120);
 var enemy2 = new Enemy( (Math.floor((Math.random() * -500) + 1) ),145, 100);
 var enemy3 = new Enemy( (Math.floor((Math.random() * -400) + 1) ),60, 80);
 var enemy4 = new Enemy( (Math.floor((Math.random() * -300) + 1) ),145, 70);
 var enemy5 = new Enemy( (Math.floor((Math.random() * -200) + 1) ),230, 90);
-
 
 var allEnemies = [
         enemy1,
@@ -223,6 +289,8 @@ var allEnemies = [
     ];
 
 var player = new Player(200,412, 0, 3);
+
+var item = new Item(-200, 200);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -235,4 +303,3 @@ document.addEventListener('keyup', function(e) {
     };
     player.handleInput(allowedKeys[e.keyCode]);
 });
-
