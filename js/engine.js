73b40b0ff5acac99b5a13d8d5da631b761fa24a7
James R <james.ruggieri@yahoo.com>
2015-08-21 23:07:06 -0700

    Add gem-green graphic to available files

--- js/engine.js
+++ js/engine.js
@@ -135,8 +135,6 @@ var Engine = (function(global) {
                 ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
             }
         }
-
-
         renderEntities();
     }
 
@@ -148,6 +146,7 @@ var Engine = (function(global) {
         /* Loop through all of the objects within the allEnemies array and call
          * the render function you have defined.
          */
+        item.render();
         allEnemies.forEach(function(enemy) {
             enemy.render();
         });
@@ -174,7 +173,8 @@ var Engine = (function(global) {
         'images/space-block.png',
         'images/ship-block.png',
         'images/enemy-alien.png',
-        'images/char-space-naut.png'
+        'images/char-space-naut.png',
+        'images/gem-green.png'
     ]);
     Resources.onReady(init);
 
